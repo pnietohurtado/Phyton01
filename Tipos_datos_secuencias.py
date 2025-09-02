a407f88d015bf9686315510543611e3806855de4
pnh0002 <pnietohurtado03@gmail.com>
2025-09-02 17:38:45 +0200

    Un ejercicio extra

--- Tipos_datos_secuencias.py
+++ Tipos_datos_secuencias.py
@@ -3,4 +3,8 @@ cadena = "información"
 for caracter in cadena: 
     print(caracter)  # Se encarga de devolver un valor de la cadena uno a uno 
 """
-
+"""
+hora = "12:23:21"
+hora.split(":")
+print(hora)
+"""
