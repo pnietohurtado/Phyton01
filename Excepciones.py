f17d21eee2578f2190bee32d42ef59e11ccd9b78
pnh0002 <pnietohurtado03@gmail.com>
2025-09-08 17:42:57 +0200

    Excepciones

---
+++ Excepciones.py
@@ -0,0 +1,19 @@
+"""
+while True: 
+    try: 
+        x = int(input("introduce un número: "))
+        break 
+    except ValueError: 
+        print("Debes introducir un número!")
+"""
+
+"""
+cad = input("Dime una cadena: ")
+try: 
+    print (10/int(cad))
+except ValueError: 
+    print ("No se puede convertir a entero!")
+except ZeroDivisionError: 
+    print("No se puede dividir entre cero!")
+"""
+
