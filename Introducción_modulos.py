a36bc7c42bffa62090124f57909f29283d7faa51
pnh0002 <pnietohurtado03@gmail.com>
2025-09-09 13:23:02 +0200

    Introducción a los modulos

---
+++ Introducción_modulos.py
@@ -0,0 +1,14 @@
+import math 
+print(math.sqrt(9))
+from math import sqrt 
+print(sqrt(9))
+from random import randint
+print(randint(1,100))
+import time 
+time.sleep(5)
+print("hola")
+
+import datetime 
+print(datetime.datetime.now())
+import os 
+os.system("clear")
