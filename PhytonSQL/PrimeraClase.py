c071dc25276b9a7b68a5dd8ea8082de197de78bc
pnh0002 <pnietohurtado03@gmail.com>
2025-09-11 23:07:31 +0200

    Pequeño repaso

---
+++ PhytonSQL/PrimeraClase.py
@@ -0,0 +1,23 @@
+nombre = "Pablo" 
+print("String: ", nombre)
+
+edad = 28 
+print("Integer: ",edad)
+
+altura = 1.65
+print("Float: ",altura)
+
+mayor_edad = True
+print("Boolean: ", mayor_edad)
+
+colores = ["rojo","verde","azul"]
+print("Lista" , colores )
+
+cliente = {"nombre" : "Luis", "edad" : 35, "activo": True}
+print("Diccionario " , cliente)
+
+coordenadas = (40.3333, -83.999)
+print("Tupla: ", coordenadas)
+
+frutas = {"manzana", "platano", "manzana", "pera"}
+print("Conjunto: ", frutas )
