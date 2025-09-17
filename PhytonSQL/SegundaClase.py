64439d262d2475046eae5dd3382edf82f7366447
pnh0002 <pnietohurtado03@gmail.com>
2025-09-17 17:00:56 +0200

    Funcionamiento de las sentencias efectuadas en python

--- PhytonSQL/SegundaClase.py
+++ PhytonSQL/SegundaClase.py
@@ -14,5 +14,34 @@ cursor.execute("""
         edad INTEGER
     )
 """)
+conexion.commit() 
 
+cursor.execute("""
+    INSERT INTO empleados(nombre, edad) VALUES ("Pepe", 12)
+""")
+conexion.commit() 
+
+cursor.execute("""
+    SELECT * FROM empleados
+""")
 conexion.commit() # Efectuar los cambios 
+
+resultados = cursor.fetchall() # Para poder sacar todos los resultados de la sentencia 
+# resultados = cursor.fetchone() # Para que devuelva únicamente uno 
+
+#for fila in resultados: 
+print(resultados)
+
+
+# Separación 
+print("########################")
+
+
+nombre = "Pepe" 
+cursor.execute("""
+    SELECT * FROM empleados WHERE nombre LIKE ?
+""", (nombre,))
+conexion.commit() 
+
+datos = cursor.fetchall() 
+print(datos)
