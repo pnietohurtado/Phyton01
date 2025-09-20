bf2ad843c7092c80c407801e055738c018ccf2d7
pnh0002 <pnietohurtado03@gmail.com>
2025-09-20 15:57:23 +0200

    Tengo problemas con el IDE de Visual, tengo que ver como usar el pyCharm

---
+++ PythonPandas/Pandas01.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+naranjas = pd.Series([4,9,2,6,10,200]) #De este modo tenemos un array que vamos a converir en serie 
+manzanas = pd.Series([600,20,30,4,5,6])
+
+print(naranjas)
