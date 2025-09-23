a56b66a3fe6aec9f5821b592a13ab1d5971e982c
pnietohurtado <pnietohurtado03@gmail.com>
2025-09-23 17:07:39 +0200

    Filtrar datos de un documentos de texto con dataframes y pandas

---
+++ PythonPandas/Pandas04.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+import xlrd 
+pd.__version__ 
+
+df = pd.read_csv("copia_titanic.csv", index_col = 0)
+print(df.head())
+print(df["Nombre"])
+
+print("-------")
+print(df.head()) 
+hombres = df[df["Sexo"] == "Masculino"]
+print(hombres)
+
+print("----------")
+print(df.head())
+id = df[df["Id_Pasajero"] > 10]
+print(id)
+
+
+print("---------")
+print(df.head())
+clase = df[df["Clase"].isin([1,2])]
+print(clase.head())
+print(clase.count() )
+
+
+print("---------")
+print(df.head()) 
+edad_nonula = df[df["Edad"].notna()]
+print(edad_nonula.head())
